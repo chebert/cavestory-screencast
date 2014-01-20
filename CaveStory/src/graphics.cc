b88a6d8d15c03636077fbea37539329e9a0bb031
Christopher Hebert <hebert.christopherj@gmail.com>
2014-01-20 15:19:24 -0800

    End of Episode 015

--- CaveStory/src/graphics.cc
+++ CaveStory/src/graphics.cc
@@ -2,16 +2,16 @@
 
 #include <SDL/SDL.h>
 
+#include "game.h"
+
 namespace {
-const int kScreenWidth = 640;
-const int kScreenHeight = 480;
 const int kBitsPerPixel = 32;
 }
 
 Graphics::Graphics() {
    screen_ = SDL_SetVideoMode(
-         kScreenWidth,
-         kScreenHeight,
+         Game::kScreenWidth,
+         Game::kScreenHeight,
          kBitsPerPixel,
          0);
    SDL_ShowCursor(SDL_DISABLE);
@@ -26,11 +26,14 @@ Graphics::~Graphics() {
    SDL_FreeSurface(screen_);
 }
 
-Graphics::SurfaceID Graphics::loadImage(const std::string& file_path) {
+Graphics::SurfaceID Graphics::loadImage(const std::string& file_path, bool black_is_transparent) {
    // if we have not loaded in the spritesheet
    if (sprite_sheets_.count(file_path) == 0) {
       // load it in now
       sprite_sheets_[file_path] = SDL_LoadBMP(file_path.c_str());
+      if (black_is_transparent) {
+         SDL_SetColorKey(sprite_sheets_[file_path], SDL_SRCCOLORKEY, 0/*black color*/);
+      }
    }
    return sprite_sheets_[file_path];
 }
