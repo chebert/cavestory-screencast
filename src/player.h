712d901c7865efc671a593a3f1c57802726d18a0
Christopher Hebert <hebert.christopherj@gmail.com>
2014-01-20 15:18:13 -0800

    End of Episode 010

--- src/player.h
+++ src/player.h
@@ -18,27 +18,47 @@ struct Player {
    void startMovingRight();
    void stopMoving();
 
+   void lookUp();
+   void lookDown();
+   void lookHorizontal();
+
    void startJump();
    void stopJump();
 
   private:
    enum MotionType {
-      STANDING,
+      FIRST_MOTION_TYPE,
+      STANDING = FIRST_MOTION_TYPE,
       WALKING,
+      JUMPING,
+      FALLING,
+      LAST_MOTION_TYPE
    };
    enum HorizontalFacing {
-      LEFT,
-      RIGHT
+      FIRST_HORIZONTAL_FACING,
+      LEFT = FIRST_HORIZONTAL_FACING,
+      RIGHT,
+      LAST_HORIZONTAL_FACING,
+   };
+   enum VerticalFacing {
+      FIRST_VERTICAL_FACING,
+      UP = FIRST_VERTICAL_FACING,
+      DOWN,
+      HORIZONTAL,
+      LAST_VERTICAL_FACING,
    };
 
    struct SpriteState {
       SpriteState(MotionType motion_type=STANDING,
-                  HorizontalFacing horizontal_facing=LEFT) :
+                  HorizontalFacing horizontal_facing=LEFT,
+                  VerticalFacing vertical_facing=HORIZONTAL) :
          motion_type(motion_type),
-         horizontal_facing(horizontal_facing) {}
+         horizontal_facing(horizontal_facing),
+         vertical_facing(vertical_facing) {}
 
       MotionType motion_type;
       HorizontalFacing horizontal_facing;
+      VerticalFacing vertical_facing;
    };
    friend bool operator<(const SpriteState& a, const SpriteState& b);
 
@@ -58,6 +78,7 @@ struct Player {
    };
 
    void initializeSprites(Graphics& graphics);
+   void initializeSprite(Graphics& graphics, const SpriteState& sprite_state);
    SpriteState getSpriteState();
 
    bool on_ground() const { return on_ground_; }
@@ -66,6 +87,7 @@ struct Player {
    float velocity_x_, velocity_y_;
    float acceleration_x_;
    HorizontalFacing horizontal_facing_;
+   VerticalFacing vertical_facing_;
    bool on_ground_;
    Jump jump_;
 
