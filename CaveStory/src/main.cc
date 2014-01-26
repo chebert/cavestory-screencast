71f66b027610ab3dbcb5bfa266a274beea818d49
Christopher Hebert <hebert.christopherj@gmail.com>
2014-01-26 15:31:00 -0800

    Fixes: OSX crash
    Original Author: RobertLowe
    Conflicts:
       CaveStory/src/main.cc
       Makefile

--- CaveStory/src/main.cc
+++ CaveStory/src/main.cc
@@ -1,4 +1,8 @@
+<<<<<<< HEAD:CaveStory/src/main.cc
 #include "SDL/SDL.h"
+=======
+#include <SDL/SDL.h> // sdl does some setup magic for osx, so it must be defined here
+>>>>>>> 41bfe0a... Fixes: OSX crash:src/main.cc
 #include "game.h"
 
 int main(int, char**) {
--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
 compile:
+<<<<<<< HEAD
 	clang++ -g -o gen/cavestory ./CaveStory/src/**.cc -lSDL
+=======
+	clang++ -o gen/cavestory src/**.cc `sdl-config --cflags --libs`
+>>>>>>> 41bfe0a... Fixes: OSX crash
 run:
 	cd gen && ./cavestory
