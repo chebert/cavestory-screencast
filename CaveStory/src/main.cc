0ebab17eafc3c78dea89dce8803a61f6b375f562
Christopher Hebert <hebert.christopherj@gmail.com>
2014-01-26 15:37:06 -0800

    fix merge conflicts
    Conflicts:
    
    	CaveStory/src/main.cc
    	Makefile

--- CaveStory/src/main.cc
+++ CaveStory/src/main.cc
@@ -1,4 +1,5 @@
-#include "SDL/SDL.h"
+#include <SDL/SDL.h> // sdl does some setup magic for osx, so it must be defined here
+>>>>>>> 59ef498... fix merge conflicts
 #include "game.h"
 
 int main(int, char**) {
--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 compile:
-	clang++ -g -o gen/cavestory ./CaveStory/src/**.cc -lSDL
+	clang++ -g -o gen/cavestory CaveStory/src/**.cc `sdl-config --cflags --libs`
 run:
 	cd gen && ./cavestory
