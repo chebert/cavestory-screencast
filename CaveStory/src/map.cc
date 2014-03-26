1a2c40943e374ad55f0ffc942276fa87f07fee30
Christopher Hebert <hebert.christopherj@gmail.com>
2014-03-25 20:06:11 -0700

    End of Episode 64

--- CaveStory/src/map.cc
+++ CaveStory/src/map.cc
@@ -5,8 +5,100 @@
 #include "graphics.h"
 #include "rectangle.h"
 
+using namespace tiles;
+
 using boost::shared_ptr;
 using std::vector;
+
+//static
+Map* Map::createSlopeTestMap(Graphics& graphics) {
+   Map* map = new Map();
+
+   map->backdrop_.reset(new FixedBackdrop("bkBlue", graphics));
+   const units::Tile num_rows = 15;
+   const units::Tile num_cols = 20;
+   // Ensure tiles_ and background_tiles_ are num_rows x num_cols in size
+   map->tiles_ = vector<vector<Tile> >(
+         num_rows, vector<Tile>(
+            num_cols, Tile()));
+   map->background_tiles_ = vector<vector<shared_ptr<Sprite> > >(
+      num_rows, vector<shared_ptr<Sprite> >(
+      num_cols, shared_ptr<Sprite>()));
+
+   Tile wall_tile(
+         TileType().set(WALL),
+         shared_ptr<Sprite>(new Sprite(
+                  graphics,
+                  "PrtCave",
+                  units::tileToPixel(1), 0,
+                  units::tileToPixel(1), units::tileToPixel(1))));
+   enum {
+      LTT,
+      LTS,
+      RTS,
+      RTT,
+      LBT,
+      LBS,
+      RBS,
+      RBT,
+      NUM_SLOPES
+   };
+   Tile slope_tiles[NUM_SLOPES];
+   for (int i = 0; i < NUM_SLOPES; ++i) {
+      slope_tiles[i] = Tile(
+            TileType().set(SLOPE).
+               set(i / 2 % 2 == 0 ? LEFT_SLOPE : RIGHT_SLOPE).
+               set(i / 4 == 0 ? TOP_SLOPE : BOTTOM_SLOPE).
+               set((i + 1) / 2 % 2 == 0 ? TALL_SLOPE : SHORT_SLOPE),
+            shared_ptr<Sprite>(new Sprite(
+                  graphics, "PrtCave",
+                  units::tileToPixel(2 + i % 4), units::tileToPixel(i / 4),
+                  units::tileToPixel(1), units::tileToPixel(1))));
+   }
+   units::Tile row = 11;
+   for (units::Tile col = 0; col < num_cols; ++col) {
+      map->tiles_[row][col] = wall_tile;
+   }
+   --row;
+   units::Tile col = 0;
+   map->tiles_[row][col++] = wall_tile;
+   map->tiles_[row][col++] = slope_tiles[LBT];
+   map->tiles_[row][col++] = slope_tiles[RBT];
+   map->tiles_[row][col++] = wall_tile;
+   map->tiles_[row][col++] = slope_tiles[LBS];
+   ++col;
+   map->tiles_[row][col++] = slope_tiles[RBS];
+   map->tiles_[row][col++] = slope_tiles[RBT];
+   map->tiles_[row][col++] = wall_tile;
+   map->tiles_[row][col++] = slope_tiles[LBT];
+   map->tiles_[row][col++] = slope_tiles[LBS];
+   map->tiles_[row][col++] = slope_tiles[RBS];
+   map->tiles_[row][col++] = slope_tiles[RBT];
+   map->tiles_[row][col] = wall_tile;
+   map->tiles_[row-1][col++] = slope_tiles[RBS];
+   map->tiles_[row][col] = wall_tile;
+   map->tiles_[row-1][col++] = slope_tiles[RBT];
+   map->tiles_[row-1][col++] = wall_tile;
+   ++col;
+   map->tiles_[row][col++] = slope_tiles[RBS];
+   map->tiles_[row][col++] = wall_tile;
+   col = 0;
+   row -= 3;
+   map->tiles_[row-1][col] = wall_tile;
+   map->tiles_[row][col++] = wall_tile;
+   map->tiles_[row-1][col] = wall_tile;
+   map->tiles_[row][col++] = slope_tiles[LTT];
+   map->tiles_[row-1][col] = wall_tile;
+   map->tiles_[row][col++] = slope_tiles[LTS];
+   map->tiles_[row-1][col] = wall_tile;
+   map->tiles_[row][col++] = slope_tiles[RTS];
+   map->tiles_[row-1][col] = wall_tile;
+   map->tiles_[row][col++] = slope_tiles[RTT];
+   map->tiles_[row-1][col] = wall_tile;
+   map->tiles_[row][col++] = wall_tile;
+
+   return map;
+}
 
 //static
 Map* Map::createTestMap(Graphics& graphics) {
@@ -28,7 +120,7 @@ Map* Map::createTestMap(Graphics& graphics) {
             "PrtCave",
             units::tileToPixel(1), 0,
             units::tileToPixel(1), units::tileToPixel(1)));
-   Tile tile(tiles::WALL_TILE, sprite);
+   Tile tile(TileType().set(WALL), sprite);
    const units::Tile row = 11;
    for (units::Tile col = 0; col < num_cols; ++col) {
       map->tiles_[row][col] = tile;
--- CaveStory/src/tile_type.h
+++ CaveStory/src/tile_type.h
@@ -1,11 +1,25 @@
 #ifndef TILE_TYPE_H_
 #define TILE_TYPE_H_
 
+#include <bitset>
+
 namespace tiles {
-   enum TileType {
-      AIR_TILE,
-      WALL_TILE
+   enum TileFlag {
+      EMPTY,
+      WALL,
+      SLOPE,
+
+      LEFT_SLOPE,
+      RIGHT_SLOPE,
+
+      TOP_SLOPE,
+      BOTTOM_SLOPE,
+
+      TALL_SLOPE,
+      SHORT_SLOPE,
+      LAST_TILE_FLAG
    };
+   typedef std::bitset<LAST_TILE_FLAG> TileType;
 }
 
 #endif // TILE_TYPE_H_
